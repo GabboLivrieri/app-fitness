f426499e2de6c566a9abcebc51975771d5ad1ae3
GiovanniFerr <giovanni.ferrandino000@gmail.com>
2026-04-20 17:51:15 +0200

    login and signup forms implementation

--- src/app/components/authentication/signup/signup.ts
+++ src/app/components/authentication/signup/signup.ts
@@ -1,9 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Auth } from '../../../auth/auth';
+import { MaterialModule } from '../../../modules/material-module';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AlertDialog } from '../../dialogs/alert-dialog/alert-dialog';
+import { AuthErrorService } from '../../../auth/auth-error';
+import { LoginForm } from '../login/login';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
-export class Signup {}
+export class SignupForm {
+  isLoading = false;
+
+  form: FormGroup;
+
+  readonly dialog = inject(MatDialog);
+
+  constructor(
+    private authService: Auth,
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+    private authError: AuthErrorService,
+    private dialogRef: MatDialogRef<SignupForm>
+  ) {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const { email, password } = this.form.value;
+    this.isLoading = true;
+
+    this.authService.register(email, password).subscribe({
+      next: () => {
+        this.isLoading = false;
+        this.dialogRef.close();
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.cdr.detectChanges();
+
+        const data = this.authError.getError(err);
+
+        this.dialog.open(AlertDialog, { data })
+      }
+    });
+  }
+
+  openLogin() {
+  this.dialogRef.close();
+  this.dialog.open(LoginForm, {
+    width: '400px',
+    disableClose: true
+  });
+}
+
+  close() {
+    this.dialogRef.close();
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginForm } from '../../components/authentication/login/login';
+import { SignupForm } from '../../components/authentication/signup/signup';
+
 
 @Component({
   selector: 'app-home',
@@ -6,4 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  constructor(private dialog: MatDialog) {}
+
+  openLogin() {
+    this.dialog.open(LoginForm, {
+      width: '400px',
+      disableClose: true
+    });
+  }
+
+  openSignup() {
+    this.dialog.open(SignupForm, {
+      width: '400px',
+      disableClose: true
+    });
+  }
+}
