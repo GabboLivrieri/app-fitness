32f048b801e36e943f0368bee77b097f5d2c2533
GiovanniFerr <giovanni.ferrandino000@gmail.com>
2026-04-20 12:40:21 +0200

    firebase and auth initialization

---
+++ src/app/auth/auth.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class Auth {
+
+  private apiKey =  "AIzaSyAu7ldXjnhs8ZHHISSzp9TXX2eTmj5rdCY";
+  private token: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  register(email: string, password: string): Observable<any> {
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apiKey}`,
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        tap((res: any) => {
+          this.token = res.idToken;
+        })
+      );
+  }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apiKey}`,
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        tap((res: any) => {
+          this.token = res.idToken;
+        })
+      );
+  }
+
+  logout() {
+    this.token = null;
+  }
+
+  isLogged(): boolean {
+    return this.token !== null;
+  }
+
+  getToken() {
+    return this.token;
+  }
+}
