53955d6344a65b2eee94ce565a90a61831db98ff
GabboLivrieri <gabriele.livrieri.005@gmail.com>
2026-04-20 14:09:26 +0200

    added navbar and footer

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Auth } from '../../../auth/auth';
+import { MaterialModule } from '../../../modules/material-module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MaterialModule, RouterLink],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
-export class Navbar {}
+export class Navbar {
+
+  constructor(
+    public auth: Auth,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/'])
+  }
+}
