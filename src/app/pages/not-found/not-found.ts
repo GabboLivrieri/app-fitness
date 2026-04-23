2e0a50a1977d9b8b51078de6b707155c5bf313cc
GiovanniFerr <giovanni.ferrandino000@gmail.com>
2026-04-23 11:26:12 +0200

    info and 404 pages

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../modules/material-module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MaterialModule, RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
